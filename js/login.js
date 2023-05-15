6e49103eb5597a46a23b31bece8c245037352113
medeirosg601 <rm552093@fiap.com.br>
2023-05-15 09:48:36 -0300

    aula js 15-05

---
+++ js/login.js
@@ -0,0 +1,20 @@
+
+ "use strict";
+
+let usuario = "";
+let senha = "";
+
+const botaoSubmit = document.querySelector("#btnSubmit");
+
+botaoSubmit.addEventListener("click", ()=>{
+    const inputUser = document.querySelector("input[name = 'nmUser']");
+    const inputPass = document.querySelector("input[name = 'pass']");
+
+    if (inputUser.value.length > 3 && inputPass.value.length > 3) {
+
+        inputUser.setAttribute("style", "outline-color: #ff0000;");
+        inputPass.setAttribute("style", "outline-color: #ff0000;");
+        
+    }
+
+});
